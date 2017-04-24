c488d8f68f6a644c4cd0e3ccd609d9933f46cd09
EmirBilgin <bilgine@itu.edu.tr>
2017-04-24 18:14:37 +0300

    deneme 7

--- skyscanner.js
+++ skyscanner.js
@@ -19,7 +19,7 @@ module.exports = {
             var data = JSON.parse(body);
 
             return data.Places.map(function (loc) {
-                return loc.PlaceId.toString();
+                return String(loc.PlaceId);
             });
         });
     },
